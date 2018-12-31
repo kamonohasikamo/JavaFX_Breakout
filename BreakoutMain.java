092094bffbcd2173f4f668b871b1b05a104568b8
cs16048 <arrowsuke@yahoo.co.jp>
2018-12-31 17:12:15 +0900

    ボールを表示させ、反射させる機能の実装

--- BreakoutMain.java
+++ BreakoutMain.java
@@ -5,6 +5,7 @@ import javafx.scene.layout.Pane;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.animation.AnimationTimer;
+import javafx.scene.paint.Color;
 
 public class BreakoutMain extends Application {
   private BreakoutThread breakoutThread;
@@ -35,7 +36,10 @@ class BreakoutThread extends AnimationTimer{
   private GraphicsContext gc;
 
   /*変数宣言*/
-  private int count;
+  private int ball_x;
+  private int ball_y;
+  private int x_speed;
+  private int y_speed;
 
   BreakoutThread(GraphicsContext gc){
     this.gc = gc;
@@ -43,7 +47,10 @@ class BreakoutThread extends AnimationTimer{
     /*コンストラクタ*/
     /*最初の一回だけ実行 -> 初期化を書く*/
 
-    count = 0;
+    ball_x = 0;
+    ball_y = 0;
+    x_speed = 5;
+    y_speed = 5;
   }
 
   @Override
@@ -52,8 +59,29 @@ class BreakoutThread extends AnimationTimer{
 
     /*ゲームループ : メインプログラム*/
 
-    gc.fillText("count = " + count, 450, 450);
-    count++;
+    gc.setFill(Color.BLACK);                    //黒のボールを生成
+    gc.fillOval(ball_x - 5, ball_y - 5, 10, 10);//ボールの位置の更新
+    //gc.fillOval(円の左上のx座標,円の左上のy座標,円の右下のx座標,円の右下のy座標);
+
+    ball_x += x_speed;
+    ball_y += y_speed;
+    if(ball_x >= 640){
+      ball_x = 640 - 5;
+      x_speed = x_speed * -1;
+    }
+    if(ball_x <= 0){
+      ball_x = 5;
+      x_speed = x_speed * -1;
+    }
+
+    if(ball_y >= 480){
+      ball_y = 480 - 5;
+      y_speed = y_speed * -1;
+    }
+    if(ball_y <= 0){
+      ball_y = 5;
+      y_speed = y_speed * -1;
+    }
   }
 
   /*追加メソッドを書く*/
