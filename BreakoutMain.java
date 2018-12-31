51762f498b77fc12029e452716c896ad46055891
cs16048 <arrowsuke@yahoo.co.jp>
2018-12-31 19:03:49 +0900

    プレイヤーバーとゲームオーバー表示、ステージ等の実装

--- BreakoutMain.java
+++ BreakoutMain.java
@@ -6,9 +6,14 @@ import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.animation.AnimationTimer;
 import javafx.scene.paint.Color;
+import javafx.scene.input.KeyEvent;
+import javafx.event.EventHandler;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
 
 public class BreakoutMain extends Application {
   private BreakoutThread breakoutThread;
+  private Key key;
 
   public static void main(String args[]){
     launch(args);
@@ -25,15 +30,68 @@ public class BreakoutMain extends Application {
     GraphicsContext gc = canvas.getGraphicsContext2D();
     pane.getChildren().add(canvas);
 
-    breakoutThread = new BreakoutThread(gc);
+    key = new Key();
+
+    scene.setOnKeyPressed(
+      new EventHandler<KeyEvent>(){
+        public void handle(KeyEvent e){
+          key.key_pressed(e);
+        }
+      }
+    );
+
+    scene.setOnKeyReleased(
+			new EventHandler<KeyEvent>(){
+				public void handle(KeyEvent e){
+					key.key_released(e);
+				}
+			}
+		);
+
+    breakoutThread = new BreakoutThread(gc, key);
     breakoutThread.start();
 
     stage.show();
   }
 }
 
+class Define{
+  private Define(){}
+
+  /*ボールの半径*/
+  public static final int BALL_R = 5;
+  /*ボールのスピード*/
+  public static final int BALL_SPEED = 5;
+
+  /*反射barのスピード*/
+  public static final int BAR_SPEED = 5;
+  /*反射barのサイズ*/
+  public static final int BAR_WIDTH = 50;
+  public static final int BAR_HEIGHT = 10;
+
+  /*フィールドサイズ*/
+  public static final int FIELD_WIDTH = 300;
+  public static final int FIELD_HEIGHT = 480;
+
+  /*ボールと自機バーの初期位置*/
+  public static final int BALL_FIRST_X = BAR_WIDTH / 2;
+  public static final int BALL_FIRST_Y = FIELD_HEIGHT - ( BAR_HEIGHT + BALL_R );
+  public static final int BAR_FIRST_X = 0;
+  public static final int BAR_FIRST_Y = FIELD_HEIGHT - BAR_HEIGHT;
+
+  /*表示位置調整*/
+  public static final int X_POSI = 170;
+}
+
 class BreakoutThread extends AnimationTimer{
   private GraphicsContext gc;
+  private Key key;
+	private Ball ball;
+	private Bar bar;
+	private Field field;
+	private CollisionDetection cd;
+	private GameOver gameover;
+	private int game_state;
 
   /*変数宣言*/
   private int ball_x;
@@ -41,17 +99,18 @@ class BreakoutThread extends AnimationTimer{
   private int x_speed;
   private int y_speed;
 
-  BreakoutThread(GraphicsContext gc){
-    this.gc = gc;
-
-    /*コンストラクタ*/
-    /*最初の一回だけ実行 -> 初期化を書く*/
-
-    ball_x = 0;
-    ball_y = 0;
-    x_speed = 5;
-    y_speed = 5;
-  }
+  BreakoutThread(GraphicsContext gc,Key key){
+		this.gc = gc;
+		this.key = key;
+		Font theFont = Font.font("Serif",FontWeight.BOLD,20);
+		gc.setFont(theFont);
+		ball = new Ball();
+		field = new Field();
+		gameover = new GameOver();
+		bar = new Bar(field,key);
+		cd = new CollisionDetection(this,ball,bar,field);
+		game_state = 0;
+	}
 
   @Override
   public void handle(long time){
@@ -59,30 +118,256 @@ class BreakoutThread extends AnimationTimer{
 
     /*ゲームループ : メインプログラム*/
 
-    gc.setFill(Color.BLACK);                    //黒のボールを生成
-    gc.fillOval(ball_x - 5, ball_y - 5, 10, 10);//ボールの位置の更新
-    //gc.fillOval(円の左上のx座標,円の左上のy座標,円の右下のx座標,円の右下のy座標);
+    key.calc_key_count();
 
-    ball_x += x_speed;
-    ball_y += y_speed;
-    if(ball_x >= 640){
-      ball_x = 640 - 5;
-      x_speed = x_speed * -1;
-    }
-    if(ball_x <= 0){
-      ball_x = 5;
-      x_speed = x_speed * -1;
-    }
-
-    if(ball_y >= 480){
-      ball_y = 480 - 5;
-      y_speed = y_speed * -1;
-    }
-    if(ball_y <= 0){
-      ball_y = 5;
-      y_speed = y_speed * -1;
-    }
+		switch(game_state){
+		  case 0:
+			  game_state = 5;
+			  break;
+		  case 5:
+			  ball.move();
+			  bar.move();
+			  cd.move();
+			  ball.draw(gc);
+			  bar.draw(gc);
+			  field.draw(gc);
+			  break;
+		  case 10:
+			  bar.draw(gc);
+			  field.draw(gc);
+			  gameover.draw(gc);
+			  break;
+		  default:
+			  break;
+		}
   }
 
   /*追加メソッドを書く*/
+  void set_game_state(int game_state){
+		this.game_state = game_state;
+	}
+
+}
+
+abstract class Base{
+	protected int width,height;
+	protected int x,y;
+	protected int x_speed;
+	protected int y_speed;
+
+	Base(){
+		width = 0;
+		height = 0;
+		x = 0;
+		y = 0;
+		x_speed = 0;
+		y_speed = 0;
+	}
+
+	int get_width(){
+		return width;
+	}
+
+	int get_height(){
+		return height;
+	}
+
+	void set_x(int x){
+		this.x = x;
+	}
+
+	void set_y(int y){
+		this.y = y;
+	}
+
+	int get_x(){
+		return x;
+	}
+
+	int get_y(){
+		return y;
+	}
+
+	abstract void init();
+	abstract void move();
+	abstract void draw(GraphicsContext gc);
+}
+
+class Field extends Base{
+  Field(){
+    width = Define.FIELD_WIDTH;
+    height = Define.FIELD_HEIGHT;
+  }
+
+  void init(){};
+  void move(){};
+
+  void draw(GraphicsContext gc){
+    gc.setStroke(Color.BLACK);
+    gc.strokeRect(Define.X_POSI + 0, 0, width, height);
+  }
+}
+
+class Ball extends Base{
+  private int r;
+
+  Ball(){
+    r = Define.BALL_R;
+    x = Define.BALL_FIRST_X;
+    y = Define.BALL_FIRST_Y;
+    x_speed = Define.BALL_SPEED;
+    y_speed = Define.BALL_SPEED;
+  }
+
+  void init(){};
+
+  int get_r(){
+    return r;
+  }
+  void ch_x_speed(){
+    x_speed *= -1;
+  }
+  void ch_y_speed(){
+    y_speed *= -1;
+  }
+
+  void move(){
+    x += x_speed;
+    y += y_speed;
+  }
+
+  void draw(GraphicsContext gc){
+    gc.setStroke(Color.BLACK);
+    gc.strokeOval(Define.X_POSI + x - r, y - r, r * 2, r * 2);
+  }
+}
+
+class Bar extends Base{
+	private Field field;
+	private Key key;
+
+	Bar(Field field,Key key){
+		this.field = field;
+		this.key = key;
+		width = Define.BAR_WIDTH;
+		height = Define.BAR_HEIGHT;
+		x = Define.BAR_FIRST_X;
+		y = Define.BAR_FIRST_Y;
+		x_speed = Define.BAR_SPEED;
+	}
+
+	void init(){}
+
+	void move(){
+		if(key.get_right() > 0 && (x + width) < field.get_width()){
+			x += x_speed;
+		}
+		if(key.get_left() > 0 && x > 0){
+			x -= x_speed;
+		}
+	}
+
+	void draw(GraphicsContext gc){
+		gc.setFill(Color.RED);
+		gc.fillRect(Define.X_POSI + x,y,width,height);
+	}
+}
+
+class CollisionDetection{
+  private BreakoutThread breakoutThread;
+  private Ball ball;
+  private Bar bar;
+  private Field field;
+
+  CollisionDetection(BreakoutThread breakoutThread, Ball ball, Bar bar, Field field){
+    this.breakoutThread = breakoutThread;
+    this.ball = ball;
+    this.bar = bar;
+    this.field = field;
+  }
+
+  void move(){
+		if(ball.get_x() >= field.get_width()){
+			ball.set_x(field.get_width() - ball.get_r());
+			ball.ch_x_speed();
+		}
+		if(ball.get_x() <= 0){
+			ball.set_x(ball.get_r());
+			ball.ch_x_speed();
+		}
+		if(ball.get_y() <= 0){
+			ball.set_y(ball.get_r());
+			ball.ch_y_speed();
+		}
+		if(ball.get_y() >= field.get_height()){
+			breakoutThread.set_game_state(10);
+		}
+		if (ball.get_x() > bar.get_x() && ball.get_x() < (bar.get_x() + bar.get_width()) &&
+			 ball.get_y() >= bar.get_y()){
+			ball.set_y(ball.get_y() - ball.get_r());
+			ball.ch_y_speed();
+		}
+	}
+}
+
+class GameOver{
+	GameOver(){}
+
+	void draw(GraphicsContext gc){
+		gc.setFill(Color.BLACK);
+		gc.fillText("GAME OVER",255,240);
+	}
+}
+
+class Key{
+	private boolean left,right;
+	private int left_count,right_count;
+
+	Key(){
+		left = false;
+		right = false;
+		left_count = 0;
+		right_count = 0;
+	}
+
+	void key_pressed(KeyEvent e){
+		switch(e.getCode()){
+		case LEFT:
+			left = true;
+			break;
+		case RIGHT:
+			right = true;
+			break;
+		default:
+			break;
+		}
+	}
+
+	void key_released(KeyEvent e){
+		switch(e.getCode()){
+		case LEFT:
+			left = false;
+			break;
+		case RIGHT:
+			right = false;
+			break;
+		default:
+			break;
+		}
+	}
+
+	void calc_key_count(){
+		if(left)left_count++;
+		else left_count = 0;
+
+		if(right)right_count++;
+		else right_count = 0;
+	}
+
+	int get_left(){
+		return left_count;
+	}
+	int get_right(){
+		return right_count;
+	}
 }
